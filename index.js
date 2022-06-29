7b9392c72b5c2dd2bf01b6979437105ffbea7328
pau <paugitonga6@gmail.com>
2022-06-29 10:05:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const exampleVar="yo!"
+
+document.querySelector("main#main").remove();
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "pau"
+newHeader.innerHTML = "PAULEEN is the champion!";
+newHeader.className = "pau";
