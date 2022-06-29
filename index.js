dbf273dbd43cf5f5e1be71d652cbaf69f4d810b6
pau <paugitonga6@gmail.com>
2022-06-29 10:07:01 +0300

    commit tree 15bf4f0bfc6c115215bb3e72f80ae64adbafe179

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const exampleVar="yo!"
 document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
-newHeader.id = "pau"
+newHeader.id = "victory"
 newHeader.innerHTML = "PAULEEN is the champion!";
 newHeader.className = "pau";
